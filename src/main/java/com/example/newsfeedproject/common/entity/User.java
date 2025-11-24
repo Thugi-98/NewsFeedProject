79c1d055d19e356d5a996ff1acc6aa4dbcc5dafa
성종민 <sjm010611@naver.com>
2025-11-24 20:48:40 +0900

    feat: 기본 엔티티 추가

--- src/main/java/com/example/newsfeedproject/common/entity/User.java
+++ src/main/java/com/example/newsfeedproject/common/entity/User.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 @Entity
 @Getter
@@ -17,12 +16,14 @@ public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
     private String email;
     private String password;
     private LocalDate birth;
     private String introduce;
 
-    public User(String email, String password, LocalDate birth, String introduce) {
+    public User(String name, String email, String password, LocalDate birth, String introduce) {
+        this.name = name;
         this.email = email;
         this.password = password;
         this.birth = birth;
