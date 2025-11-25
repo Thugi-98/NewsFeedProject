f7c0df4915a1e88d162bc04e3a6b6f4c32abf68b
이용준 <abc157898@gmail.com>
2025-11-25 12:44:16 +0900

    refactor: 전역 예외처리 클래스 코드 수정 / LYJ 공용 응답 객체(ApiResponse)를 사용하기 위한 코드 수정

--- src/main/java/com/example/newsfeedproject/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/newsfeedproject/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.example.newsfeedproject.common.exception;
 
-import com.example.newsfeedproject.common.dto.ErrorCode;
+import com.example.newsfeedproject.common.dto.ApiResponse;
 import com.example.newsfeedproject.common.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,50 +11,58 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    //커스텀 예외처리
+    /**
+     * 커스텀 예외 처리
+     */
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
-        ErrorCode errorCode = ex.getErrorCode();
+    public ResponseEntity<ApiResponse<?>> handleCustomException(CustomException e) {
+
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(ApiResponse.error(new ErrorResponse(
+                        e.getErrorCode().getStatus().value(), e.getErrorCode().name(), e.getMessage())));
+    }
+
+    /**
+     * 검증 예외 처리
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException e) {
+
+        String defaultMessage = null;
+        String field = null;
+
+        if (e.getBindingResult().getFieldError() != null) {
+            defaultMessage = e.getBindingResult().getFieldError().getDefaultMessage();
+            field = e.getBindingResult().getFieldError().getField();
+        }
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                e.getStatusCode().value(),                // HTTP status code
+                field != null ? field : "VALIDATION_ERROR", // 어떤 필드에서 오류 났는지 표시
+                defaultMessage != null ? defaultMessage : "Invalid request" // 실제 메시지
+        );
+
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(ApiResponse.error(errorResponse));
+    }
+
+
+    /**
+     * 나머지 예외 서버에러 처리
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
 
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .status(errorCode.getStatus().value())
-                .code(errorCode.getStatus().name())
-                .message(errorCode.getMessage())
-                .build();
-
-        return ResponseEntity.status(errorCode.getStatus()).body(errorResponse);
-    }
-
-    // @Valid 실패 처리 (Validation 예외)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleValidationException(
-            MethodArgumentNotValidException ex
-    ) {
-        String message = "유효성 검사에 실패하였습니다.";
-        if (ex.getBindingResult().getFieldError() != null) {
-        }
-        message = ex.getBindingResult().getFieldError().getDefaultMessage();
-
-        ErrorResponse response = ErrorResponse.builder()
-                .status(400)
-                .code("BAD_REQUEST")
-                .message(message)
-                .build();
-
-        return ResponseEntity.badRequest().body(response);
-
-    }
-
-    // 나머지 예외 서버에러 처리
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
-        ErrorResponse response = ErrorResponse.builder()
-                .status(500)
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .code("INTERNAL_SERVER_ERROR")
                 .message("서버 문제로 인해 실패하였습니다.")
                 .build();
 
-        return ResponseEntity.internalServerError().body(response);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error(errorResponse));
     }
-
 }
