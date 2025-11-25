2af19315175210032d2c23eae29ef11d94ac18c0
Jeongha Im <yimjh3205@gmail.com>
2025-11-25 14:29:12 +0900

    feat: (@NoArgsConstructor 추가) / IJH

--- src/main/java/com/example/newsfeedproject/user/dto/request/CreateUserRequest.java
+++ src/main/java/com/example/newsfeedproject/user/dto/request/CreateUserRequest.java
@@ -2,10 +2,12 @@ package com.example.newsfeedproject.user.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Getter
+@NoArgsConstructor
 public class CreateUserRequest {
     private String name;
     private String email;
