06b4b4a29c1dfd8a39e9c1bd8d37d9d200a281d9
Jeongha Im <yimjh3205@gmail.com>
2025-11-26 16:41:45 +0900

    feat: 계정 비공개 여부 필드 추가 / IJH

--- src/main/java/com/example/newsfeedproject/user/dto/request/UpdateUserRequest.java
+++ src/main/java/com/example/newsfeedproject/user/dto/request/UpdateUserRequest.java
@@ -1,6 +1,7 @@
 package com.example.newsfeedproject.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,6 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class UpdateUserRequest {
 
-    @NotBlank(message = "사용자 이름은 반드시 입력해야 합니다.")
     @Size(min = 1, max = 30, message = "사용자 이름은 1자 이상 30자 이하여야 합니다.")
     private String name;
 
@@ -20,10 +20,13 @@ public class UpdateUserRequest {
     @NotBlank(message = "현재 비밀번호는 필수 입력값입니다.")
     private String currentPassword;
     // 새로운 비밀번호
-    private String password;
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8자 이상 16자 이하 영문 대소문자, 숫자, 특수문자를 사용해야 합니다.")
+    private String newPassword;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birth;
     @Size(max = 100, message = "소개글은 100자 이하여야 합니다.")
     private String introduction;
+
+    private Boolean followPrivate;
 }
