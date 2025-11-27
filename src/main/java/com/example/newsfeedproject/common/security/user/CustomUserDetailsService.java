6aace89d40534910fbd8926ff16bb32e493f65fa
JH319 <imjh3205@gmail.com>
2025-11-28 01:32:24 +0900

    Update CustomUserDetailsService.java
    줄 맞추기

--- src/main/java/com/example/newsfeedproject/common/security/user/CustomUserDetailsService.java
+++ src/main/java/com/example/newsfeedproject/common/security/user/CustomUserDetailsService.java
@@ -21,10 +21,11 @@ public class CustomUserDetailsService implements UserDetailsService {
     // email을 이용해 사용자 정보를 조회하고 사용자 정보 DTO 반환(부모 타입 반환 - 다형성)
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmailAndIsDeletedFalse(email).orElseThrow(() -> {
+        User user = userRepository.findByEmailAndIsDeletedFalse(email)
+            .orElseThrow(() -> {
                     log.error("[Not Found User] 유저를 찾을 수 없습니다.");
                     return new UsernameNotFoundException(ErrorCode.NOT_FOUND_USER.getMessage());
-                });
+            });
 
         // Spring Security가 이해하는 UserDetails(일종의 DTO)로 변환해서 리턴
         return new CustomUserDetails(user);
