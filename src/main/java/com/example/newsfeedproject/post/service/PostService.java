9fe860b3667696f0942695f763ee89048522cfe3
이용준 <abc157898@gmail.com>
2025-11-28 00:40:43 +0900

    feat : flush() 로직 추가

--- src/main/java/com/example/newsfeedproject/post/service/PostService.java
+++ src/main/java/com/example/newsfeedproject/post/service/PostService.java
@@ -101,6 +101,7 @@ public class PostService {
         }
 
         post.update(request.getTitle(), request.getContent());
+        postRepository.flush();
 
         return PostUpdateResponse.from(post);
     }
